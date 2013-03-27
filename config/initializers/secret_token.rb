d310eaaeec12ac6a8a2d1fecfee7a48205b8b955
Nicholas Pan <nwpan@sfu.ca>
2013-03-27 09:17:44 -0700

    init home rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Home::Application.config.secret_token = 'cfea2783a18e0d10c3f4482e08bba9a2ada9a9bb6f7ca51efe75ccc7d77e32bcac88f47f106e7a2b4eea51fdd5400242ec7ea91f1deb68e192aaf87f185ab077'
