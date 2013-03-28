47e2b5018c5963526339b65109dd3c124e575add
Nicholas Pan <nwpan@sfu.ca>
2013-03-27 22:41:12 -0700

    beta fixes

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module Home
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.paths << Rails.root.join("beta", "app", "assets")
   end
 end
